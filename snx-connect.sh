5a2bf18c08c39a38ac084f394da507e0a98b4796
Igor Suzart <igorarsuzart@gmail.com>
2025-07-20 23:42:01 -0300

    Fix command execution in snx-connect.sh by removing erroneous flatpak run command

--- snx-connect.sh
+++ snx-connect.sh
@@ -3,4 +3,4 @@
 
 # O Flatpak instala os seus ficheiros Python em /app/bin/
 # O comando 'exec' substitui o processo do shell pelo processo do Python
-exec python3 /app/src/main.py "$@"flatpak run com.github.igorsuzart.snx_connect
+exec python3 /app/src/main.py "$@"
